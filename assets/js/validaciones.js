1f03abd544738fe779791f4aa79e16cce5a6c2cb
Luciano Urtubia <luciano.conectividadyredes.ti@gmail.com>
2021-05-30 20:49:23 -0400

    aver una prueba

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -3,7 +3,7 @@ const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    usuario: /^[9-]{9,10}$/, // Letras, numeros, guion y guion_bajo
+    usuario: /^\d{7,14}$/, // Letras, numeros, guion y guion_bajo
     password: /^.{4,12}$/, // 4 a 12 digitos.
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
     telefono: /^\d{7,14}$/ // 7 a 14 numeros.
